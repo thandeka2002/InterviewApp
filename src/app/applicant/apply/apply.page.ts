31f6c162e4a1a5f6462d0c3a26a762ad41042f66
NKULULEKO NKOMO <22014247@live.mut.ac.za>
2023-08-29 17:09:26 +0200

    commitment

--- src/app/applicant/apply/apply.page.ts
+++ src/app/applicant/apply/apply.page.ts
@@ -51,9 +51,13 @@ export class ApplyPage implements OnInit {
   studentno='';
   qualification = '';
   graduationYear = '';
-  universityOrCollege = '';
+  Qdescription='';
+  degree= '';
+  studyField= '';
+  universityOrCollege= ''; 
   workEperience = '';
   workReferance = '';
+  TotalExperience = '';
   academicRecordURl = '';
   certicatesUrl = '';
   idURL = '';
@@ -93,6 +97,7 @@ export class ApplyPage implements OnInit {
   codeError: any;
   fullNameError:any;
   lastNameError:any;
+  TotalExperienceError: any;
   coursedata : any[]=[];
   provinceError:any;
  bithDateError:any;
@@ -214,6 +219,7 @@ municipalities:any[]=[];
     this.universityError=null
     this.yearError=null;
     this.refError=null;
+    this.TotalExperienceError=null;
 
    
   
@@ -329,6 +335,12 @@ municipalities:any[]=[];
       alert('Please enter your grade average.');
       return;
    }
+
+   if (this.TotalExperience === '') {
+    this.TotalExperienceError = 'Please enter your Country.';
+    alert('Please enter your country.');
+    return;
+  }
     
    const gradeAverageRegex = /^\d{1,3}$/; // regular expression for one to three-digit numbers
     
@@ -520,6 +532,7 @@ municipalities:any[]=[];
           references: this.references,
           qualifications: this.qualifications,
           gradeAverage:this.gradeAverage,
+          tTotalExperience: this.TotalExperience,
           skills: this.skills,
           license: this.license,
           languages: this.languages,
@@ -547,7 +560,7 @@ municipalities:any[]=[];
 
         loader.dismiss();
         alert('Information successfully saved');
-        this.navCtrl.navigateForward('/home');
+        this.navCtrl.navigateForward('/view');
       } else {
         loader.dismiss();
         throw new Error('User not found');
@@ -684,6 +697,7 @@ municipalities:any[]=[];
                 qualifications:this.qualifications,
                 gradeAverage:this.gradeAverage,
                 skills: this.skills,
+                TotalExperience: this.TotalExperience,
                 languages: this.languages,
                 graduationYear: this.graduationYear,
                 status: 'pending',
@@ -716,7 +730,7 @@ municipalities:any[]=[];
         loader.dismiss();
         alert('Information successfully saved');
         //this.navCtrl.navigateForward('/home');
-        this.navCtrl.navigateForward('/profile', {
+        this.navCtrl.navigateForward('/view', {
         queryParams: { reference: this.fullname, data: this.data, source: 'buttons' },
         });
       } else {
@@ -1282,7 +1296,7 @@ console.log('update cv ' + this.cvUrl);
             this.province = userDocument.province;
             this.graduationYear = userDocument.graduationYear;
             this.faculty = userDocument.faculty;
-            
+            this.TotalExperience = userDocument.TotalExperience;
             this.level = userDocument.level;
             this.selectedOption = userDocument.course;
         
